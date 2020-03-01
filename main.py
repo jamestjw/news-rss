f3bb1baa66cfb5ae3e0a69c9d19d4c6978853cb9
James Tan <james_tan97@outlook.com>
2020-03-01 22:42:29 +0800

    Rename function handler to readRSS

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ logger.setLevel(level)
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def handler(request, callback):
+def readRSS(request, callback):
     secret = get_secret()
     DB_URL = f"mongodb+srv://{secret['DB_USER']}:{secret['DB_PW']}@{secret['CLUSTER_NAME']}.gcp.mongodb.net/test?retryWrites=true&w=majority"
     DB_CONN = pymongo.MongoClient(DB_URL).get_database(secret['DB_NAME'])
