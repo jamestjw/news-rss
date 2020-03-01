a022cf320997e3ccfe4054e9ae01bb42a09a38a8
James Tan <james_tan97@outlook.com>
2020-03-01 12:11:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pymongo
+import ssl
+import re
+from helper import get_secret, RSSReader, DatabaseAdapter
+import logging
+
+logger = logging.getLogger("Handler")
+level = logging.getLevelName('INFO')
+logger.setLevel(level)
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+URL = 'https://gnews.io/api/v3/topics/{}'
+
+secret = get_secret()
+DB_URL = f"mongodb+srv://{secret['DB_USER']}:{secret['DB_PW']}@{secret['CLUSTER_NAME']}.gcp.mongodb.net/test?retryWrites=true&w=majority"
+DB_CONN = pymongo.MongoClient(DB_URL).get_database(secret['DB_NAME'])
+
+def handler(request, callback):
+    db = DatabaseAdapter(DB_CONN, name = 'news')
+    RSSReader(db).fetch_and_write()
+
---
+++ reader.py
@@ -0,0 +1,64 @@
+import re
+import feedparser
+import logging
+
+logger = logging.getLogger("Handler")
+level = logging.getLevelName('INFO')
+logger.setLevel(level)
+
+RSS_DICT = {
+    'tech': 'https://www.google.com/alerts/feeds/01736057707602744226/6181175218216352531'
+}
+
+class RSSReader:
+    def __init__(self, db, topic = 'tech', collection_name = 'news'):
+        assert topic in RSS_DICT.keys(), f"Your topic must be one of the following: {RSS_DICT.keys()}."
+        self.topic = topic
+        self.url = RSS_DICT[topic]
+        self.id_regex = re.compile('feed\:(.*)$')
+        self.url_regex = re.compile(r'url=(.*)&ct=')
+        self.db = db
+        logger.info(f'Initialised with topic : {topic}')
+    
+    def make_request(self):
+        logger.info(f'Retrieving RSS feed from {self.url}')
+        response = feedparser.parse(self.url)
+        entries = response['entries']
+
+        logger.info(f'{len(entries)} were found...')
+
+        data = [self.filter_dict(entry, ['id', 'link', 'title', 'summary' , 'published']) for entry in entries]
+        return data
+
+    def filter_dict(self, dirty_dict, keys):
+        res = {k:v for k,v in dirty_dict.items() if k in keys}
+        res['id'] = self.parse_id(res['id'])
+        res['link'] = self.parse_url(res['link'])
+        res['topic'] = self.topic
+        return res
+
+    def parse_id(self, id_string):
+        match = self.id_regex.search(id_string)
+        return match[1] if match else id_string
+
+    def parse_url(self, url_string):
+        match = self.url_regex.search(url_string)
+        return match[1] if match else url_string
+
+    def fetch_and_write(self):
+        docs = self.make_request()
+        data = [doc for doc in docs if self.db.id_not_exists(doc['id'])]
+        self.db.write_many_to_db(data)
+
+class DatabaseAdapter:
+    def __init__(self, db_conn, name):
+        self.conn = db_conn
+        self.name = name
+        self.collection = self.conn.get_collection(name)
+    
+    def write_many_to_db(self, data):
+        logger.info(f'Writing {len(data)} items to the {self.name} collection...')
+        self.collection.insert_many(data)
+
+    def id_not_exists(self, id):
+        return self.collection.count_documents({'id': id}) == 0
