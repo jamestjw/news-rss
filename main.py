bbcf93da3e89390a0f448724697af1df1387aeaa
James Tan <james_tan97@outlook.com>
2020-03-01 22:02:30 +0800

    Move database connection handler block

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ logger.setLevel(level)
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-secret = get_secret()
-DB_URL = f"mongodb+srv://{secret['DB_USER']}:{secret['DB_PW']}@{secret['CLUSTER_NAME']}.gcp.mongodb.net/test?retryWrites=true&w=majority"
-DB_CONN = pymongo.MongoClient(DB_URL).get_database(secret['DB_NAME'])
-
 def handler(request, callback):
+    secret = get_secret()
+    DB_URL = f"mongodb+srv://{secret['DB_USER']}:{secret['DB_PW']}@{secret['CLUSTER_NAME']}.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    DB_CONN = pymongo.MongoClient(DB_URL).get_database(secret['DB_NAME'])
+
     for topic in TOPICS.keys():
         db = DatabaseAdapter(DB_CONN, col_name = topic)
         RSSReader(db, topic=topic).fetch_and_write()
