72e29727ede4f6ce821a69db40aa6b035b272bfb
James Tan <james_tan97@outlook.com>
2020-03-08 19:10:40 +0800

    Add start message

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ logger.setLevel(level)
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def readRSS(request, callback):
+    logger.info('Function execution starting...')
     secret = get_secret()
     DB_URL = f"mongodb+srv://{secret['DB_USER']}:{secret['DB_PW']}@{secret['CLUSTER_NAME']}.gcp.mongodb.net/test?retryWrites=true&w=majority"
     DB_CONN = pymongo.MongoClient(DB_URL).get_database(secret['DB_NAME'])
